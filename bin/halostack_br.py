949c6a199a968f2060c753740cd5bac0a5088ec4
Panu Lahtinen <pnuu+git@iki.fi>
2015-03-26 18:44:10 +0200

    Example script showing how to calculate B-R image (invented by Lefadeux) using Halostack

---
+++ bin/halostack_br.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from halostack.image import Image
+import sys
+import os
+
+# input file
+fname_in = sys.argv[1]
+
+# form the output filename
+head, tail = os.path.split(fname_in)
+tail = tail.split('.')[:-1]
+tail.append('png')
+tail = 'br_' + '.'.join(tail)
+out_fname = os.path.join(head, tail)
+
+# the actual halostack BR work is three lines
+
+# read image
+img = Image(fname=fname_in)
+# gradient removal and B-R maneuver
+img.enhance({'gradient': None, 'br': None})
+# save the resulting image
+img.save(out_fname)
