c1fa08198f936606bf0c7c74c7659acdc05d1334
Panu Lahtinen <pnuu+git@iki.fi>
2026-09-06 22:39:36 +0300

    Raise version to 0.5.0

--- halostack/__init__.py
+++ halostack/__init__.py
@@ -23,7 +23,7 @@
 """Image stacking for ice crystal halo photographs."""
 
 __major__ = "0"
-__minor__ = "4"
+__minor__ = "5"
 __patch__ = "0"
 
 __version__ = "v" + ".".join([__major__, __minor__, __patch__])
